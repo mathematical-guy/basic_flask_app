93ba65d8dc9fdf849ba9aa449dcf1c2a2ab9b2e8
Mohit <mohittalrejaceh@gmail.com>
2021-03-19 02:37:02 +0530

    Initial Commit

---
+++ myapp.py
@@ -0,0 +1,44 @@
+import numpy as np
+from flask import Flask, render_template, request
+app = Flask(__name__)
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+
+def classifier(datapoint):
+    iris = load_iris()
+    X = iris.data
+    Y = iris.target
+    knn = KNeighborsClassifier(n_neighbors = 1)
+    knn.fit(X, Y)
+    datapoint = np.array(datapoint).reshape(1, -1)
+    return knn.predict(datapoint)
+
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.htm')
+
+@app.route('/result', methods = ["POST"])
+def result():
+    sepal_length = float(request.form.get('sepal_length'))
+    sepal_width = float(request.form.get('sepal_width'))
+    petal_length = float(request.form.get('petal_length'))
+    petal_width = float(request.form.get('petal_width'))
+    datapoint = [sepal_length, sepal_width, petal_length, petal_width]
+    result = classifier(datapoint)[0]
+    print(datapoint)
+    print(result)
+    if result == 0:
+            result = "Setossa"
+    elif result == 1:
+        result = 'Versicolor'
+    else:
+        result = 'Virginica'    
+    
+    return render_template("result.htm", result = result)
+
+app.run(debug=True)
+
+
+
